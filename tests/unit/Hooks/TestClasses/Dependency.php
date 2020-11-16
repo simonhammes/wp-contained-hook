68b2c7094a59f8040866feb5e6fea9bf9ec52bed
Simon Hammes <simonhammes.27@gmail.com>
2020-11-16 03:36:19 +0100

    Create Dependency.php

---
+++ tests/unit/Hooks/TestClasses/Dependency.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace TypistTech\WPContainedHook\Test\Hooks\TestClasses;
+
+class Dependency 
+{
+    public function doSomething(): string 
+    {
+        return 'response';
+    }
+}
+
+?>
